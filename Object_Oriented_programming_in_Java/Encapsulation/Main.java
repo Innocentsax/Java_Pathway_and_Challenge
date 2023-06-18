d34b710b1916af3fbc8f93aff2d757db0c510c1f
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-06-18 13:38:06 +0100

    Update Main.java

--- Object_Oriented_programming_in_Java/Encapsulation/Main.java
+++ Object_Oriented_programming_in_Java/Encapsulation/Main.java
@@ -4,22 +4,24 @@ public class Main {
     public static void main(String[] args) {
 
         // ENCAPSULATION ON PLAYER WITHOUT ENCAPSULATION
-//        Player_without_Encapsulation player = new Player_without_Encapsulation();
-//        player.name = "Innocent";
-//        player.health = 20;
-//        player.weapon = "Sword";
-//
-//        int damage = 10;
-//        player.loseHealth(damage);
-//        System.out.println("Remaining health = " + player.healthRemaining());
-//
-//        player.loseHealth(11);
-//        System.out.println("Remaining health = " + player.healthRemaining());
+        Player_without_Encapsulation player = new Player_without_Encapsulation();
+        player.name = "Innocent";
+        player.health = 20;
+        player.weapon = "Sword";
+
+        int damage = 10;
+        player.loseHealth(damage);
+        System.out.println("Remaining health = " + player.healthRemaining());
+
+        player.loseHealth(11);
+        System.out.println("Remaining health = " + player.healthRemaining());
 
         //ENCAPSULATION ON PLAYER WITH ENCAPSULATION
-//        Player_with_Encapsulation inno = new Player_with_Encapsulation("Inno");
-//        System.out.println("Initial health is " + inno.healthRemaining());
-
+        Player_with_Encapsulation inno = new Player_with_Encapsulation("Inno");
+        System.out.println("Initial health is " + inno.healthRemaining());
+        
+        
+        // ENCAPSULATION CHALLENGE
         Printer printer = new Printer(50, true);
         System.out.println("Initial page count = " + printer.getPagesPrinted());
 
@@ -28,5 +30,7 @@ public class Main {
 
         pagesPrinted = printer.printPages(10);
         System.out.printf("Current Job Pages: %d, Printer Total: %d %n", pagesPrinted, printer.getPagesPrinted());
+        
+        
     }
 }
