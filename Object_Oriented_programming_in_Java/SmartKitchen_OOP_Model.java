526f5c05fdbe683e3e115e6d16f6a51fd52bca13
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-06-18 09:52:57 +0100

    SmartKitchen_OOP_Model

---
+++ Object_Oriented_programming_in_Java/SmartKitchen_OOP_Model.java
@@ -0,0 +1,104 @@
+public class SmartKitchen {
+    private CoffeeMaker brewMaster;
+    private Refrigerator iceBox;
+    private DishWasher dishWasher;
+
+    public SmartKitchen() {
+        brewMaster = new CoffeeMaker();
+        iceBox = new Refrigerator();
+        dishWasher = new DishWasher();
+    }
+
+    public CoffeeMaker getBrewMaster() {
+        return brewMaster;
+    }
+
+    public Refrigerator getIceBox() {
+        return iceBox;
+    }
+
+    public DishWasher getDishWasher() {
+        return dishWasher;
+    }
+
+    public void setKitchenState(boolean coffeeFlag, boolean fridgeFlag, boolean dishWasherFlag){
+        brewMaster.setHasWorkToDo(coffeeFlag);
+        iceBox.setHasWorkToDo(fridgeFlag);
+        dishWasher.setHasWorkToDo(dishWasherFlag);
+    }
+
+    public void doKitchenWork(){
+        brewMaster.brewCoffee();
+        iceBox.orderFood();
+        dishWasher.doDishes();
+    }
+}
+
+class CoffeeMaker{
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void brewCoffee(){
+        if (hasWorkToDo){
+            System.out.println("Brewing Coffee");
+            hasWorkToDo = false;
+        }
+    }
+}
+
+class Refrigerator{
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void orderFood(){
+        if (hasWorkToDo){
+            System.out.println("Ordering Food");
+            hasWorkToDo = false;
+        }
+    }
+}
+
+class DishWasher{
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void doDishes(){
+        if (hasWorkToDo){
+            System.out.println("Washing Dishes");
+            hasWorkToDo = false;
+        }
+    }
+}
+
+
+
+// MAIN FUNCTION
+public class Main {
+    public static void main(String[] args) {
+        SmartKitchen kitchen = new SmartKitchen();
+
+        kitchen.getDishWasher().setHasWorkToDo(true);
+        kitchen.getIceBox().setHasWorkToDo(true);
+        kitchen.getBrewMaster().setHasWorkToDo(true);
+
+
+        kitchen.getDishWasher().doDishes();
+        kitchen.getIceBox().orderFood();
+        kitchen.getBrewMaster().brewCoffee();
+
+        System.out.println("\n\n\n\n\n");
+
+
+        kitchen.setKitchenState(true, false, true);
+        kitchen.doKitchenWork();
+    }
+}
